944322199c939d3fa3c912c226a3f2c24685ae03
Steven Maresca <steve@zentific.com>
2015-03-20 06:36:47 -0400

    Update options parsing to take lat/lon as separate field names implicitly. Retab whitespace, update copyright. Improve error messaging.

--- haversine/bin/haversine.py
+++ haversine/bin/haversine.py
@@ -1,5 +1,7 @@
-# Copyright 2012 - Steven Maresca
-# steve.maresca@gmail.com
+# Copyright 2012-2015 - Steven Maresca
+# steve@zentific.com
+#
+# Haversine: a Splunk command for computing great-circle distance between two points.
 
 import os, splunk.Intersplunk, logging as logger
 import math
@@ -22,16 +24,16 @@ def haversine(origin, point):
 
     return d
 
-def toPoint(str):
-    if str and "," in str:
-        originpoint = str.split(',')
+def toPoint(pt):
+    if pt and "," in pt:
+        originpoint = pt.split(',')
 
         if len(originpoint) != 2:
             return None
-        
+
         try:
-                originpoint[0] = float(originpoint[0].strip())
-                originpoint[1] = float(originpoint[1].strip())
+            originpoint[0] = float(originpoint[0].strip())
+            originpoint[1] = float(originpoint[1].strip())
         except ValueError:
             return None
 
@@ -39,12 +41,11 @@ def toPoint(str):
 
     return None
 
-
-def main():
+def run_command():
     try:
         messages = {}
         keywords, options = splunk.Intersplunk.getKeywordsAndOptions()
-        results,dummyresults,settings = splunk.Intersplunk.getOrganizedResults()
+        results, dummyresults, settings = splunk.Intersplunk.getOrganizedResults()
 
         outputField = options.get('outputField', 'distance')
         units = options.get('units', 'km')
@@ -55,19 +56,31 @@ def main():
         inputFieldLat = options.get('inputFieldLat', None)
         inputFieldLon = options.get('inputFieldLon', None)
 
-        #pull input field(s)
-        if len(keywords) == 0 and not inputFieldLon and not inputFieldLat:
-            return splunk.Intersplunk.generateErrorResults("Calculation not performed; required input field name is missing.")
-        elif len(keywords) == 1:
+        # Pull input fields (if and only if inputField is not set):
+        #   single interpreted as "lat,lon" in decimal degree format
+        #   two interpreted as lat lon, respectively
+        #   >2 ignored
+        num_keywords = len(keywords)
+        if num_keywords > 2:
+            return splunk.Intersplunk.generateErrorResults("Calculation not performed; too many keywords, expected two at most (latitude and longitude).")
+
+        if ( inputField or inputFieldLat or inputFieldLon ) and num_keywords > 0 and num_keywords < 3 :
+            return splunk.Intersplunk.generateErrorResults("Calculation not performed; latitude and longitude field names are ambiguous. Specify one fieldname for decimal-degree format data or two fields for latitude and longitude, but not both.")
+
+        if num_keywords == 1:
             inputField = keywords[0]
-        
-        #TODO consider input lat/lon if 2 keywords present
-        #elif len(keywords) == 2:
-        #    inputFieldLat = keywords[0]
-        #    inputFieldLon = keywords[1]
+        elif num_keywords == 2:
+            inputFieldLat = keywords[0]
+            inputFieldLon = keywords[1]
+
+        if (not inputFieldLat or not inputFieldLon) and not inputField:
+            return splunk.Intersplunk.generateErrorResults("Calculation not performed; latitude and longitude field name(s) not provided to haversine.")
+
+        if inputFieldLat and inputFieldLon and inputField:
+            return splunk.Intersplunk.generateErrorResults("Calculation not performed; latitude and longitude field names are ambiguous. Specify one fieldname for decimal-degree format data or two fields for latitude and longitude, but not both.")
 
         if not originValue and not originField:
-            return splunk.Intersplunk.generateErrorResults("Calculation not performed; required origin field value/name is missing.")
+            return splunk.Intersplunk.generateErrorResults("Calculation not performed; required origin value/field name is missing.")
 
         if results:
             for result in results:
@@ -89,9 +102,9 @@ def main():
                     return splunk.Intersplunk.generateErrorResults("Origin value malformed. Received '%s' - expected origin='x,y' as a value represented using decimal degree notation, (e.g. '-41.22,80.22')." % origin)
 
 
-                inputCoord = "" 
+                inputCoord = ""
 
-                # build input coordinates from what we 
+                # build input coordinates from what we
                 # have available (inputField with lat,lon tuple or two separate fields)
                 if inputFieldLat and inputFieldLon:
                     if inputFieldLat not in result or inputFieldLon not in result:
@@ -104,42 +117,40 @@ def main():
                 else:
                     if inputField not in result:
                         continue
-                
+
                     if not result[inputField]:
                         continue
-                
+
                     inputCoord = result[inputField]
-                
+
                 inputPoint = toPoint(inputCoord)
-                
-		# geoip outputs lat/lon that are empty if details unknown (or if invalid inputField used)
-                # so in this case, handle it gracefully. 
+
+                # geoip outputs lat/lon that are empty if details unknown (or if invalid inputField used)
+                # so in this case, handle it gracefully.
                 #
-                # >1 rather than >0 to simultaneously account for the case where 
+                # >1 rather than >0 to simultaneously account for the case where
                 #  inputCoord (from a single inputField) is == "" OR
                 #  inputCoord="," when inputFieldLat+","+inputFieldLon=="," because they're blank
-                #  
-		if not inputPoint and len(inputCoord) > 1:
+                #
+                if not inputPoint and len(inputCoord) > 1:
                     return splunk.Intersplunk.generateErrorResults("Input coordinates malformed. Received '%s' - expected inputField='x,y' as a value represented using decimal degree notation, (e.g. '-41.22,80.22')." % inputCoord)
                 elif not inputPoint:
                     #silently ignore lat,lon=""
                     continue
-                
+
                 distance = haversine(originPoint, inputPoint)
-                
+
                 if type(distance) == float and units=="mi":
                     result[outputField] = distance*0.621371192
                 elif type(distance) == float:
                     result[outputField] = distance
-        
-        splunk.Intersplunk.outputResults(results)
-        
+
+            splunk.Intersplunk.outputResults(results)
+
     except Exception, e:
         import traceback
         stack =  traceback.format_exc()
         splunk.Intersplunk.generateErrorResults(str(e))
         logger.error(str(e) + ". Traceback: " + str(stack))
 
-if __name__ == '__main__':
-    main()
-
+run_command()
